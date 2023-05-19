be74a7d38d150b3cd69be9f5ff1197dc33d08acf
ri-commits <kartashourychard@gmail.com>
2023-05-19 18:28:10 +0300

    SubHeading

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,3 +1,10 @@
-const SubHeading = () => <div>SubHeading</div>;
+import { images } from "../../constants";
+
+const SubHeading = ({ title }) => (
+  <div style={{ marginBottom: "1rem" }}>
+    <p className="p__cormarant">{title}</p>
+    <img src={images.spoon} alt="spoon" className="spoon__img" />
+  </div>
+);
 
 export default SubHeading;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-
-import './Header.css';
+import "./Header.css";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new Flavour" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+    </div>
+    <div className="app__wrapper_img"></div>
   </div>
 );
 
