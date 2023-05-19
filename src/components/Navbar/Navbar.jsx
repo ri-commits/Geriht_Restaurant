84626bfbf7d74180e5cce8eeafb3e7b079e8dc3e
ri-commits <kartashourychard@gmail.com>
2023-05-19 10:56:40 +0300

    Navbar JSX

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,32 @@
 import "./Navbar.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 
-const Navbar = () => <div>Navbar</div>;
+import images from "../../constants/images";
+
+const Navbar = () => (
+  <nav className="app__navbar">
+    <div className="app__navbar-logo">
+      <img src={images.gericht} alt="app logo" />
+    </div>
+    <ul className="app__navbar-links">
+      <li className="p__opensans">
+        <a href="#home">Home</a>
+      </li>{" "}
+      <li className="p__opensans">
+        <a href="#about">About</a>
+      </li>{" "}
+      <li className="p__opensans">
+        <a href="#menu">Menu</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#awards">Awards</a>
+      </li>
+      <li className="p__opensans">
+        <a href="#contact">Contact</a>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Navbar;
